3b39faf4a232ad2ca8e4a7eeb29b0b30934b0f7b
harakoryunosuke <ryunosuke493@gmail.com>
2022-08-04 15:31:11 +0900

    商品出品機能の導入3

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,7 +21,7 @@ class Item < ApplicationRecord
     validates :shipping_cost_id
     validates :prefecture_id
     validates :shipping_date_id
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
   with_options numericality: { other_than: 0 } do
